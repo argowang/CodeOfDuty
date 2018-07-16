0263bb4cdb779fa89b40e77e7b9fc67805a26139
Candice-X <494118806@qq.com>
2018-07-16 01:20:17 -0400

    faster method

--- 20180715/46.java
+++ 20180715/46.java
@@ -25,3 +25,30 @@ class Solution {
         nums[j] = tmp;
     }
 }
+
+import java.util.Arrays;
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        List<Integer> current = new ArrayList<Integer>();
+        helper(nums, result, current);
+        return result;
+    }
+    private void helper(int[] nums, List<List<Integer>> result, List<Integer> current) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<Integer>(current));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(current.contains(nums[i])) {
+                continue;
+            }
+            current.add(nums[i]);
+            helper(nums, result, current);
+            current.remove(current.size() - 1);
+        }
+    }
+}
