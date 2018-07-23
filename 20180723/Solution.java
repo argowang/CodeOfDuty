b60be832204204bc657d777317042cbfd1286c1c
chenKuer <chenfjk@gmail.com>
2018-07-24 00:44:07 +0800

    add: 9. palidrome numbers

--- 20180723/Solution.java
+++ 20180723/Solution.java
@@ -1,4 +1,7 @@
 class Solution {
+  /*
+  * 7. reverse a number
+  */
     public int reverse(int x) {
         if (x>Integer.MAX_VALUE || x<Integer.MIN_VALUE) {
             return 0;
@@ -17,4 +20,29 @@ class Solution {
         }
         return rev;
     }
+
+
+    /*
+    * 9. palidrome Numbers
+    */
+
+    public boolean isPalindrome(int x) {
+        int original = x;
+        int rev = 0;
+        if (x < 0) {
+            return false;
+        } else if (x ==0 ) {
+            return true;
+        }
+        while( x!= 0 ) {
+
+            rev = rev * 10 + x%10;
+            x = x /10;
+        }
+        if (rev == original) {
+            return true;
+        }
+        return false;
+    }
+
 }
