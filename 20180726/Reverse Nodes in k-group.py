f9f711b22b23bde2c0a5abc677321e3ec762f87b
DorothyHYY <Doro_hyy@163.com>
2018-07-27 00:19:03 +0800

    Python version for 20180726

--- 20180726/Reverse Nodes in k-group.py
+++ 20180726/Reverse Nodes in k-group.py
@@ -57,4 +57,31 @@ class Solution(object):
         
             
         
-        
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        cur = head
+        count = 0
+        while cur != None and count != k:
+            cur = cur.next
+            count += 1
+        if count == k:
+            cur = self.reverseKGroup(cur, k)
+            while count != 0:
+                tmp = head.next
+                head.next = cur
+                cur = head
+                head = tmp
+                count -= 1
+            head = cur
+        return head        
